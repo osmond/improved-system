f2a06e1c8a3370d0bf0a9f3e797549eb63e5741c
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-31 12:15:05 -0500

    Remove bar chart examples from examples page

--- src/pages/Examples.tsx
+++ src/pages/Examples.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import BarChartExamples from "@/components/examples/BarChartExamples";
 import AreaChartInteractive from "@/components/examples/AreaChartInteractive";
 
 export default function Examples() {
   return (
     <div className="grid gap-6">
       <AreaChartInteractive />
-      <BarChartExamples />
     </div>
   );
 }
