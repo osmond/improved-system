de98967aee3245707065aacf9124d37c0b7a1405
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-31 13:54:41 -0500

    Revert "Display Garmin data on Examples page"

--- src/pages/Examples.tsx
+++ src/pages/Examples.tsx
@@ -1,26 +1,31 @@
 import React from "react";
-import {
-  StepsChart,
-  PaceDistributionChart,
-  HeartRateZonesChart,
-} from "@/components/dashboard";
-import { useGarminData } from "@/hooks/useGarminData";
-import { useRunningStats } from "@/hooks/useRunningStats";
+import AreaChartInteractive from "@/components/examples/AreaChartInteractive";
+import LineChartInteractive from "@/components/examples/LineChartInteractive";
+import BarChartInteractive from "@/components/examples/BarChartInteractive";
+
+import ChartRadarDefault from "@/components/examples/RadarChartDefault";
+import ChartRadialSimple from "@/components/examples/RadialChartSimple";
+import ChartRadialLabel from "@/components/examples/RadialChartLabel";
+import ChartRadialText from "@/components/examples/RadialChartText";
 
 
 export default function Examples() {
-  const garmin = useGarminData();
-  const running = useRunningStats();
-
-  if (!garmin || !running) {
-    return <p>Loading…</p>;
-  }
-
   return (
     <div className="grid gap-6">
-      <StepsChart />
-      <PaceDistributionChart data={running.paceDistribution} />
-      <HeartRateZonesChart data={running.heartRateZones} />
+      <AreaChartInteractive />
+
+      <BarChartInteractive />
+
+      <LineChartInteractive />
+
+      <ChartRadarDefault />
+
+      <ChartRadialSimple />
+
+      <ChartRadialLabel />
+
+      <ChartRadialText />
+
     </div>
   );
 }
