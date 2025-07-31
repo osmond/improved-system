bb53899811ceb6cb54f56131799cedebe400b88a
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-31 12:19:30 -0500

    Add interactive bar chart example

--- src/pages/Examples.tsx
+++ src/pages/Examples.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import BarChartExamples from "@/components/examples/BarChartExamples";
 import AreaChartInteractive from "@/components/examples/AreaChartInteractive";
+import ChartBarInteractive from "@/components/examples/BarChartInteractive";
 
 export default function Examples() {
   return (
     <div className="grid gap-6">
       <AreaChartInteractive />
+      <ChartBarInteractive />
       <BarChartExamples />
     </div>
   );
