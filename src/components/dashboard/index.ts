c9d54caa1d423285c1bb2965dbb97b9a984cac4b
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-31 15:11:53 -0500

    Add ProgressRingWithDelta component and integrate

--- src/components/dashboard/index.ts
+++ src/components/dashboard/index.ts
@@ -1,6 +1,7 @@
 
 export { default as ChartCard } from "./ChartCard";
 export * from "./ProgressRing";
+export * from "./ProgressRingWithDelta";
 export * from "./ActivitiesChart";
 export * from "./StepsChart";
 export * from "./DailyStepsChart";
