867abeb0862a63f024309e114712e0332236052b
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-31 12:24:30 -0500

    Add pie chart donut example

--- src/pages/Examples.tsx
+++ src/pages/Examples.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import BarChartExamples from "@/components/examples/BarChartExamples";
 import AreaChartInteractive from "@/components/examples/AreaChartInteractive";
 import LineChartInteractive from "@/components/examples/LineChartInteractive";
+import PieChartDonutText from "@/components/examples/PieChartDonutText";
 
 export default function Examples() {
   return (
     <div className="grid gap-6">
       <AreaChartInteractive />
       <LineChartInteractive />
+      <PieChartDonutText />
       <BarChartExamples />
     </div>
   );
