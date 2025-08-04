00b492a3b2d9c38d6c4e4353ec51ab9a52bf62eb
Jon Osmond <jonathan.osmond@gmail.com>
2025-08-04 18:10:16 -0500

    Add navigational breadcrumbs component

--- src/layouts/MobileTabLayout.tsx
+++ src/layouts/MobileTabLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MobileTabBar from "@/components/layout/MobileTabBar";
+import Breadcrumbs from "@/components/layout/Breadcrumbs";
 
 interface MobileTabLayoutProps {
   children: React.ReactNode;
@@ -8,7 +9,10 @@ interface MobileTabLayoutProps {
 export default function MobileTabLayout({ children }: MobileTabLayoutProps) {
   return (
     <div className="flex h-full flex-col">
-      <div className="flex-1 overflow-auto">{children}</div>
+      <div className="flex-1 overflow-auto p-4">
+        <Breadcrumbs />
+        {children}
+      </div>
       <MobileTabBar className="md:hidden" />
     </div>
   );
