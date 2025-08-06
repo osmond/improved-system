ab6ee4cc6e0cfd06fd0f42d4ba020039cc189f44
Jon Osmond <jonathan.osmond@gmail.com>
2025-08-06 07:17:48 -0500

    fix: await reading speed script

--- scripts/generate-reading-speed.js
+++ scripts/generate-reading-speed.js
@@ -2,9 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const { getReadingSpeed } = require('../server/services/kindleService');
 
-function main() {
-  const data = getReadingSpeed();
-  const outPath = path.join(__dirname, '..', 'src', 'data', 'kindle', 'reading-speed.json');
+async function main() {
+  const data = await getReadingSpeed();
+  const outPath = path.join(
+    __dirname,
+    '..',
+    'src',
+    'data',
+    'kindle',
+    'reading-speed.json'
+  );
   fs.writeFileSync(outPath, JSON.stringify(data, null, 2) + '\n');
   console.log(`Wrote ${data.length} records to ${outPath}`);
 }
